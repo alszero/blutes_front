4ea78bc9f1de30ebd06938edbf3eb257f3aea6b4
alszero <alszero05@gmail.com>
2023-06-14 03:18:44 +0900

    커뮤니티페이지

--- src/pages/community/CommunityPage.jsx
+++ src/pages/community/CommunityPage.jsx
@@ -1,104 +1,172 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { RiHeart2Line, RiChat1Line } from 'react-icons/ri'; // 하트와 댓글 아이콘 추가
 import Header from "../Header.js";
 
-const PageContainer = styled.div`
+// 스타일드 컴포넌트 정의
+const SearchContainer = styled.div`
+  position: relative;
+  width: 700px;
+  margin: 50px auto;
   display: flex;
-  flex-direction: column;
   align-items: center;
 `;
 
-const Heading = styled.h1`
-  margin-bottom: 20px;
+const SearchInput = styled.input`
+  width: 100%;
+  border: 1px solid #bbb;
+  border-radius: 8px;
+  padding: 10px 12px;
+  font-size: 14px;
+  border-radius: 20px;
+`;
+
+const SearchIcon = styled.img`
+  position: absolute;
+  width: 17px;
+  top: 50%;
+  right: 20%;
+  transform: translateY(-50%);
+  margin: 0;
 `;
 
 const PostButton = styled.button`
   padding: 10px 20px;
-  background-color: #4caf50;
+  margin-left: 10px;
+  background-color: #071DA1;
   color: #fff;
   border: none;
   border-radius: 3px;
   font-size: 16px;
+  word-break: keep-all;
   cursor: pointer;
   transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #45a049;
-  }
 `;
 
-const Table = styled.table`
-  width: 100%;
-  border-collapse: collapse;
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  flex-grow: 1;
+`;
+
+const WriteDate = styled.p`
+  margin: 0 0 0 460px; // 왼쪽 마진만 설정
+  text-align: right;
+`;
+
+const Content = styled.p`
+  text-align: center;
+  margin-top: 40px;
+`;
+
+const View = styled.p`
+  align-self: flex-start;
+  margin-bottom: 0;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 8px; /* 아이콘과 조회수 간격 조정 */
+  align-self: flex-end; /* 아이콘을 오른쪽으로 정렬 */
+`;
+
+const HeartIcon = styled(RiHeart2Line)`
+  margin-right: 13px;
+`;
+
+const CommentIcon = styled(RiChat1Line)`
+  margin-right: 8px;
+`;  
+
+const PageContainer = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 600px;
+  height: auto;
+  border: 1px solid #E0E0E0;
+  margin-left: auto;
+  margin-right: 10px; /* Updated margin-right */
+  padding: 10px;
+`;
+
+const TodayContainer = styled.div`
+  margin-top: 40px;
+  width: 200px;
+  height: 200px;
+  border: 1px solid #E0E0E0;
+  margin-left: 10px; /* Updated margin-left */
+  margin-right: auto;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const Todate = styled.div`
   margin-top: 20px;
+  margin-left: 20px;
+  color: #E0E0E0;
 `;
 
-const TableHead = styled.thead`
-  background-color: #f2f2f2;
+const TodayVisitor = styled.div`
+  margin-top: 10px;
+  margin-left: 20px; 
 `;
 
-const TableHeader = styled.th`
-  padding: 10px;
-  text-align: left;
+const VisitorCount = styled.p`
+  margin-left: 20px; 
+  color: #071DA1;
 `;
 
-const TableBody = styled.tbody``;
-
-const TableRow = styled.tr`
-  &:nth-child(even) {
-    background-color: #f9f9f9;
-  }
+const TodayComment = styled.p`
+  margin-top: 10px;
+  margin-left: 20px; 
 `;
 
-const TableCell = styled.td`
-  padding: 10px;
+const CommenterCount = styled.p`
+  margin-left: 20px;
+  color: #071DA1;
 `;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: #000;
-`;
-
 const CommunityPage = () => {
-  const posts = [
-    { id: 1, author: '사용자1', date: '2023-06-10', title: '글 제목 1', views: 100 },
-    { id: 2, author: '사용자2', date: '2023-06-09', title: '글 제목 2', views: 75 },
-    { id: 3, author: '사용자3', date: '2023-06-08', title: '글 제목 3', views: 50 },
-    { id: 4, author: '사용자4', date: '2023-06-07', title: '글 제목 4', views: 25 }
-  ];
-
   return (
     <div>
       <Header />
+      <SearchContainer>
+        <SearchInput type="text" placeholder="검색어 입력" />
+        <SearchIcon src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png" />
+        <PostButton>작성하기</PostButton>
+      </SearchContainer>
+      <hr />
+      <Wrapper>
         <PageContainer>
-          <Heading>커뮤니티 페이지</Heading>
-          <Link to="/post">
-            <PostButton>작성하기</PostButton>
-          </Link>
-          <Table>
-            <TableHead>
-              <tr>
-                <TableHeader>작성자</TableHeader>
-                <TableHeader>작성일</TableHeader>
-                <TableHeader>제목</TableHeader>
-                <TableHeader>조회수</TableHeader>
-              </tr>
-            </TableHead>
-            <TableBody>
-              {posts.map(post => (
-                <TableRow key={post.id}>
-                  <TableCell>{post.author}</TableCell>
-                  <TableCell>{post.date}</TableCell>
-                  <TableCell>
-                    <StyledLink to={`/post/${post.id}`}>{post.title}</StyledLink>
-                  </TableCell>
-                  <TableCell>{post.views}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <TitleWrapper>
+            <Title className='Title'>제목</Title>
+            <WriteDate className='WriteDate'>작성일자</WriteDate>
+          </TitleWrapper>
+          <Content className='Content'>내용</Content>
+          <View className='View'>조회수 0</View>
+          <IconWrapper>
+            <CommentIcon />
+            <HeartIcon />
+          </IconWrapper>
+          <hr/>
         </PageContainer>
+        <TodayContainer>
+          <Todate className='Todate'>2023.06.14</Todate>
+          <TodayVisitor className='TodayVisitor'>오늘의 방문자</TodayVisitor>
+          <VisitorCount className='VisitorCount'>00명</VisitorCount>
+          <TodayComment className='TodayComment'>오늘의 포스트/답글</TodayComment>
+          <CommenterCount className='CommenterCount'>00명</CommenterCount>
+        </TodayContainer>
+    </Wrapper>
     </div>
   );
 };
--- src/pages/dictionary/dictionary.jsx
+++ src/pages/dictionary/dictionary.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Main } from './pages/dictionary/Main';
 
 const VocabularyList = ({ wordList }) => {
   return (
